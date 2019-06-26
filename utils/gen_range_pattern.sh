6148f44c8e1d605b1ce8513e921930cfef3be1be
S. Chauveau <schauveau@nvidia.com>
2019-06-26 16:52:25 +0200

    Fix color depth in script

--- utils/gen_range_pattern.sh
+++ utils/gen_range_pattern.sh
@@ -1,7 +1,14 @@
+#
+# Generate a gradient checkboard patter suitable to detect
+# color range issues (clipping in highlight and shadows).
+#
+
 convert -negate -size 512x512 pattern:checkerboard A.png
 convert -size 512x512 gradient: B.png
 convert \
     -negate -size 512x512 pattern:checkerboard \
     -size 512x512 gradient: \
-    -compose Mathematics -define compose:args="0,1,0.2,-0.1" -composite -define png:color-type=2 range-pattern.png
+    -compose Mathematics -define compose:args="0,1,0.2,-0.1" -composite \
+    -depth 8 -define png:color-type=2 range-pattern.png
+
 identify range-pattern.png
